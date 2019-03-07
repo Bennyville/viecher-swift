013faa3c1a0e02869d003aa3be3de8950cdf483e
Benny Peckruhn <bennypeck@gmail.com>
2019-03-07 14:51:11 +0100

    Initial Commit

---
+++ Viecher/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Viecher
+//
+//  Created by Benny Peckruhn on 07.03.19.
+//  Copyright © 2019 Benny Peckruhn. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
